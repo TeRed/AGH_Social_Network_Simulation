376083deb51947101a56d40e64e29e0925ce889b
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2018-12-25 23:10:42 +0100

    lil tweak

--- src/features/navbar/NavBar.js
+++ src/features/navbar/NavBar.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 import "./NavBar.css";
-import { Text } from '../../components/Text/Text';
+import { Text } from "../../components/Text/Text";
 
 export const NavBar = () => (
-    <div className="NavBar">
-        <Text size={2}>Social Network Evolution</Text>
-    </div>
+  <div className="NavBar">
+    <Text className={"navbar-text"} size={2}>
+      Social Network Evolution
+    </Text>
+  </div>
 );
