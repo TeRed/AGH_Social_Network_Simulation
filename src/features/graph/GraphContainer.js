1b6d827b643dae467ba656a3808fcb3d2f7defc6
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2018-12-26 01:58:08 +0100

    some animations

--- src/features/graph/GraphContainer.js
+++ src/features/graph/GraphContainer.js
@@ -11,112 +11,86 @@ export default class GraphContainer extends React.Component {
   nodesSleep = [];
   nodesDead = [];
   T = 20;
+  currentT = 1;
 
   nodesAll = [];
   edgesAll = [];
 
   render() {
-    let cyStyle = {
-      height: "100vh",
-      width: "100%"
-    };
-    return <div style={cyStyle} id="graph" />;
+    return <div className={"GraphContainer"} id="cy" />;
   }
 
   componentDidMount() {
     this.CY = cytoscape({
-      container: document.getElementById("graph"),
-      elements: this.simulate(),
+      container: document.getElementById("cy"),
       layout: {
         name: "cose"
       }
     });
+
+    setTimeout(() => {
+      for (let t = 1; t < this.T; t++) {
+        setTimeout(() => this.simulate(t), t * 1000);
+      }
+    }, 0);
   }
 
-  simulate = () => {
-    for (let t = 1; t < this.T; t++) {
-      // punkt 1
-      // Oni mają do tego funkcje ale ona zwraca strasznie duze liczby
-      let nodes = Array.apply(null, Array(CyUtil.N(t))).map(() => ({
-        id: uniqid()
-      }));
+  simulate = t => {
+    // punkt 1
+    // Oni mają do tego funkcje ale ona zwraca strasznie duze liczby
+    let nodes = Array.apply(null, Array(CyUtil.N(t))).map(() => ({
+      id: uniqid()
+    }));
 
-      // punkt 2
-      nodes.forEach(n => {
-        n.death_time = CyUtil.lifetime() + t;
-      });
+    this.CY.add(nodes.map(el => ({ group: "nodes", data: { id: el.id } })));
 
-      //punkt 3  - zakładam, że wybieramy tylko ze śpiących
-      nodes.forEach(n => {
-        if (this.nodesSleep.length === 0) {
-          //w pierwszej iteracji nie wybieram polaczen
-          n.links = [];
-        } else {
-          // console.log(t);
-          // console.log(this.nodesSleep);
-          let linkNode = CyUtil.lookForManyLinksNode(this.nodesSleep);
-          // console.log(linkNode);
-          n.links = [linkNode];
-          linkNode.links.push(n); //linki w dwie strony
-        }
-      });
-
-      //punkt 4
-      nodes.forEach(n => {
-        n.wake_time = CyUtil.sleeptime() + t;
-      });
-
-      //punkt 5 - trzeba sprawdzić, żebyśmy nie powtarzali node'ów
-      let nodesAwaken = this.nodesSleep.filter(n => n.wake_time <= t);
-
-      nodesAwaken.forEach(n => {
-        let linkNode = CyUtil.getRandomLink(n);
-        if (linkNode !== n) {
-          n.links.push(linkNode);
-          linkNode.links.push(n);
-        }
-      });
-
-      //punkt 6
-      let newNodesDead = nodesAwaken.filter(n => n.death_time <= t);
-      //Usuwamy nowe martwe node'y ze ze śpiących
-      this.nodesSleep = this.nodesSleep.filter(n => !newNodesDead.includes(n));
-      this.nodesDead = this.nodesDead.concat(newNodesDead);
-
-      this.nodesSleep = this.nodesSleep.concat(nodes);
-
-      console.log("Iteration: " + t);
-      console.log(this.nodesDead.length);
-      console.log(this.nodesSleep.length);
-    }
-
-    //Przetwarzanie danych dla cytoscape
-    this.nodesSleep.forEach(n => {
-      this.nodesAll.push({ data: { id: n.id } });
-
-      let mainId = n.id;
-
-      if (!!n.links)
-        n.links.forEach(ln => {
-          if (!!ln) {
-            this.edgesAll.push({ data: { source: mainId, target: ln.id } });
-          }
-        });
+    // punkt 2
+    nodes.forEach(n => {
+      n.deathTime = CyUtil.lifetime() + t;
     });
 
-    this.nodesDead.forEach(n => {
-      this.nodesAll.push({ data: { id: n.id } });
-
-      let mainId = n.id;
-
-      if (!!n.links)
-        n.links.forEach(ln => {
-          if (!!ln) {
-            this.edgesAll.push({ data: { source: mainId, target: ln.id } });
-          }
-        });
+    //punkt 3  - zakładam, że wybieramy tylko ze śpiących
+    nodes.forEach(n => {
+      if (this.nodesSleep.length === 0) {
+        //w pierwszej iteracji nie wybieram polaczen
+        n.links = [];
+      } else {
+        // console.log(t);
+        // console.log(this.nodesSleep);
+        let linkNode = CyUtil.lookForManyLinksNode(this.nodesSleep);
+        // console.log(linkNode);
+        n.links = [linkNode];
+        linkNode.links.push(n); //linki w dwie strony
+      }
     });
 
-    return { nodes: this.nodesAll, edges: this.edgesAll };
+    //punkt 4
+    nodes.forEach(n => {
+      n.wakeTime = CyUtil.sleeptime() + t;
+    });
+
+    //punkt 5 - trzeba sprawdzić, żebyśmy nie powtarzali node'ów
+    let nodesAwaken = this.nodesSleep.filter(n => n.wakeTime <= t);
+
+    nodesAwaken.forEach(n => {
+      let linkNode = CyUtil.getRandomLink(n);
+      if (linkNode !== n) {
+        n.links.push(linkNode);
+        linkNode.links.push(n);
+        this.CY.add({
+          group: "edges",
+          data: { source: n.id, target: linkNode.id }
+        });
+      }
+    });
+
+    //punkt 6
+    let newNodesDead = nodesAwaken.filter(n => n.deathTime <= t);
+    //Usuwamy nowe martwe node'y ze ze śpiących
+    this.nodesSleep = this.nodesSleep.filter(n => !newNodesDead.includes(n));
+    this.nodesDead = this.nodesDead.concat(newNodesDead);
+
+    this.nodesSleep = this.nodesSleep.concat(nodes);
+    this.CY.layout({ name: "cose", animate: 'end', componentSpacing: 15 }).run();
   };
 }
